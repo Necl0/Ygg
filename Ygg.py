c04bea702dc7c2615e9be04977dda7fc2fc2b26c
Josiah <63004866+Necloremius@users.noreply.github.com>
2022-08-19 22:22:08 -0400

    Update Ygg.py

--- Ygg.py
+++ Ygg.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import glob
 import cv2
-
+import pickle
 import matplotlib.pyplot as plt
 
 from sklearn import metrics
@@ -19,18 +19,17 @@ labels = {'astilbe': 1,'bellflower': 2,'black_eyed_susan': 3,'calendula': 4,'cal
 imgs = []
 y = []
 
-
 for flower in labels:
   for filename in glob.glob(f"/content/drive/MyDrive/archive/flowers/{flower}/*.jpg"): 
       #read in image as greyscale
       img = cv2.imread(filename, 0)
 
       # resize to 50x50
-      img = cv2.resize(img, (50,50))
+      img = cv2.resize(img, (75,75))
       y.append(labels[flower])
       imgs.append(img)
-
-
+          
+          
 x_train, x_test, y_train, y_test = train_test_split(imgs, y, test_size=0.2)
 
 x_train = np.array(x_train)
@@ -54,3 +53,12 @@ y_predict = clf.predict(x_test2d)
 # compare the predicted labels to the actual labels and conver to percentage to output
 acc = metrics.accuracy_score(y_test, y_predict)*100
 print(f"{acc}% accuracy")
+
+# saving the model to a pickle file
+with open('model.pkl','wb') as f:
+    pickle.dump(clf,f)
+
+# ------------------------------------------- #
+# example of loading the model
+with open('model.pkl', 'rb') as f:
+    clf2 = pickle.load(f)
